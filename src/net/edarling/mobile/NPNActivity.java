80cc07eb8026f01372efa41b285f6b1887654ffc
tmaus <tmaus@affinitas.de>
2012-03-30 09:46:38 +0200

    added notification and toast

--- src/net/edarling/mobile/NPNActivity.java
+++ src/net/edarling/mobile/NPNActivity.java
@@ -1,13 +1,17 @@
 package net.edarling.mobile;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.phonegap.DroidGap;
 
 public class NPNActivity extends DroidGap
 {
-    /** Called when the activity is first created. */
-    @Override
+
     public void onCreate(Bundle savedInstanceState)
     {
         NPNPlugin.getInstance();
@@ -15,4 +19,6 @@ public class NPNActivity extends DroidGap
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
     }
+
+
 }
--- src/net/edarling/mobile/NPNC2DMReceiver.java
+++ src/net/edarling/mobile/NPNC2DMReceiver.java
@@ -1,10 +1,23 @@
 package net.edarling.mobile;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,7 +31,8 @@ public class NPNC2DMReceiver extends BroadcastReceiver {
     private static String KEY = "c2dmPref";
     private static String REGISTRATION_KEY = "registrationKey";
     private final String TAG = "NPNC2DMReceiver";
-
+    private final String LS_PAYLOAD_KEY = "notification_payload";
+    
     
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG,"onReceive with action:" + intent.getAction()+"!");
@@ -26,7 +40,43 @@ public class NPNC2DMReceiver extends BroadcastReceiver {
         if (intent.getAction().equals("com.google.android.c2dm.intent.REGISTRATION")) {
             handleRegistration(context, intent);
         } else if (intent.getAction().equals("com.google.android.c2dm.intent.RECEIVE")) {
-            NPNPlugin.getInstance().deliverNotification(intent.getExtras());
+
+            // the message we want to display
+            CharSequence tickerText;
+
+            try{
+                tickerText = intent.getExtras().get("message").toString();
+            }catch(NullPointerException npe){
+                tickerText = "Dude, no key=message has been provided. ";
+            }
+
+            // create status-bar notification
+            NotificationManager nm = (NotificationManager) context.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            int icon = R.drawable.icon_small;
+            Notification notification = new Notification(icon, tickerText, System.currentTimeMillis());
+            Intent mainIntent = new Intent(Intent.ACTION_MAIN);
+            mainIntent.setClass(context, NPNActivity.class);
+            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // Intent that is called once user clicks on notification
+            PendingIntent contentIntent = PendingIntent.getActivity(context.getApplicationContext(), 0, mainIntent, 0);
+            notification.setLatestEventInfo(context, "eDarling", tickerText, contentIntent);
+            nm.notify(1, notification);
+
+            // display a toast to the user to inform him on the incoming notification
+            LayoutInflater inflater = LayoutInflater.from(context.getApplicationContext());
+            View layout = inflater.inflate(R.layout.notification, null);
+            ImageView image = (ImageView) layout.findViewById(R.id.image);
+            image.setImageResource(R.drawable.icon_small);
+            TextView text = (TextView) layout.findViewById(R.id.text);
+            text.setText(tickerText);
+            Toast toast = new Toast(context.getApplicationContext());
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(layout);
+            toast.show();
+
+
         }else {
         
         Log.w(TAG,"action: " + intent.getAction() + " does not match");
